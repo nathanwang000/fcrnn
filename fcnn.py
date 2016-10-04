42b6e5ae47ac9fbbee152dbad26f14a7be757c39
Jiaxuan Wang <nathanwang000@gmail.com>
2016-10-04 00:31:31 -0400

    pass grad check from output

--- fcnn.py
+++ fcnn.py
@@ -18,9 +18,7 @@ class FCNN: # fully connected nueral network
         assert(time > 0)
 
         # values
-        self.I = np.zeros((self.nin, self.bs))
-        self.H = np.zeros((self.nhid, self.bs))
-        self.O = np.zeros((self.nout, self.bs))
+        self.resetValue()
 
         # connections: m for mask, g for gradient, b for bias
         self.mIH = np.ones((self.nhid, self.nin))
@@ -42,7 +40,15 @@ class FCNN: # fully connected nueral network
         # activation
         self.Hact = act
 
+    def resetValue(self):
+        self.I = np.zeros((self.nin, self.bs))
+        self.H = np.zeros((self.nhid, self.bs))
+        self.O = np.zeros((self.nout, self.bs))
+        self.pH = np.zeros((self.nhid, self.bs))
+        self.pI = np.zeros((self.nin, self.bs))
+
     def resetGrad(self):
+
         self.gIH = np.zeros((self.nhid, self.nin))
         self.gIO = np.zeros((self.nout, self.nin))
         self.gHH = np.zeros((self.nhid, self.nhid))
@@ -76,14 +82,14 @@ class FCNN: # fully connected nueral network
     def openErrGate(self, batch, loss):
         # TODO
         # run loss.forward on output to get l
-        target = np.zeros_like(self.O)
+        target = self.O.copy()
         for i in range(len(batch.y)):
             target[:,i] = batch.y[i].reshape(target[:,i].shape)
         l = loss.forward(self.O, target)
         # run loss.backward on output to get dE/dO
-        # error = loss.backward(self.O, target).\
-        #         reshape((1,self.nout,self.bs))
-        error = np.zeros((1,self.nout,self.bs))
+        error = loss.backward(self.O, target).\
+                reshape((1,self.nout,self.bs))
+        # error = np.zeros((1,self.nout,self.bs))
         # assume row major
         for i in range(self.bs):
             err = error[...,i]
@@ -101,7 +107,6 @@ class FCNN: # fully connected nueral network
         return l
         
     def inputFeeder(self, batch):
-        # TODO: gracefully handle last batch!
         def feed():
             I = np.zeros((self.nin, self.bs))
             for i in range(len(batch.x)):
@@ -132,27 +137,118 @@ class FCNN: # fully connected nueral network
         _gbH = np.zeros((self.nout, self.nhid, self.bs))
         
         for i in range(self.bs):
-            H, I = self.H[:,i], self.I[:,i]
+            H, I = self.H[:,i], self.pI[:,i]
             self._gHO[...,i] = bd(*([H] * self.nout))
             self._gIO[...,i] = bd(*([I] * self.nout))
             # MAYBE: handle variable length output            
             _gIH[...,i] = self.HO.dot(self._gIH[...,i])
             _gHH[...,i] = self.HO.dot(self._gHH[...,i])
             _gbH[...,i] = self.HO.dot(self._gbH[...,i])
+
         self._gIH, self._gHH, self._gbH = _gIH, _gHH, _gbH
+        self.O = self.IO.dot(self.pI)+self.HO.dot(self.H)+self.bO
+
+    def randValue(self):
+        self.I = np.random.random((self.nin, self.bs))
+        self.H = np.random.random((self.nhid, self.bs))
+        self.O = np.random.random((self.nout, self.bs))
+        
+    def gradCheck(self):
+        t = np.random.randint(1,3)
+        
+        bs = self.bs
+        self.bs = 1
+
+        self.randValue()
+        I = self.I.copy()
+        H = self.H.copy()
+        O = self.O.copy()
+        self.resetGrad()
+        fh = []
+        for _ in range(t):
+            self.step()
+            fh.append(self.H.copy())
+        self.freezeStep()
+        self.resetGrad()                
+        fo = self.O.copy()
+        fh.append(self.H.copy())
+        
+        def checkW(name):
+            entity = getattr(self, name)
+            self.I = I.copy()
+            self.H = H.copy()
+            self.O = O.copy()
+            self.resetGrad()
+            delta = 1e-7
+            x = np.random.randint(entity.shape[0])
+            y = np.random.randint(entity.shape[1])
+            o = np.random.randint(self.nout)
+            entity[x,y] += delta
+
+            for i in range(t):
+                self.step()
+                # g_entity = getattr(self, "_g"+name)
+                # gf = (self.H - fh[i]) / delta
+                # egf = g_entity[:, x*entity.shape[1]+y, 0]
+                # print(H[0] * self.Hact.backward(self.pH[0]))
+                # print(name + " h:", gf, egf) 
+            self.freezeStep()
+
+            g_entity = getattr(self, "_g"+name)            
+            foa = self.O.copy()
+            gf = (foa-fo)/delta
+            egf = g_entity[o, x*entity.shape[1]+y, 0]
+            print(name + ":", gf[o], egf)
+            entity[x,y] -= delta
+
+        def checkB(name):
+            if not name.startswith('b'): name = "b" + name
+            entity = getattr(self, name)
+            self.I = I.copy()
+            self.H = H.copy()
+            self.O = O.copy()
+            self.resetGrad()
+            delta = 1e-6
+            x = np.random.randint(entity.shape[0])
+            o = np.random.randint(self.nout)            
+            entity[x] += delta
+
+            for _ in range(t):
+                self.step()
+            self.freezeStep()
+
+            g_entity = getattr(self, "_g"+name)
+            foa = self.O.copy()
+            gf = (foa-fo)/delta
+            egf = g_entity[o, x, 0]
+            print(name + ":", gf[o], egf)
+            entity[x] -= delta
+        
+        checkW("IO")
+        checkW("HO")
+        checkW("IH") # for t > 1 not working          
+        checkW("HH") # for t > 1 not working
+        checkB("H") # no need to check O as it must be right
+
+        self.bs = bs
+        self.resetGrad()
+        self.resetValue()
         
     def step(self): # forward 1 timestep
+        nI = np.zeros_like(self.I)
+        nH = self.IH.dot(self.I) + self.HH.dot(self.H) + self.bH
+        nO = self.IO.dot(self.I) + self.HO.dot(self.H) + self.bO
+        self.pH = nH # before activation
 
         def forward():
-            nI = np.zeros_like(self.I)
-            nH = self.IH.dot(self.I) + self.HH.dot(self.H) + self.bH
-            nO = self.IO.dot(self.I) + self.HO.dot(self.H) + self.bO
+            self.pI = self.I # previous I
             self.I, self.H, self.O = nI, self.Hact.forward(nH), nO
 
         def accGrad():
             for i in range(self.bs):
-                H, I = self.H[...,i], self.I[...,i]
-                hback = np.diag(self.Hact.backward(H))
+                pH, I = self.pH[...,i], self.I[...,i] # ph need new
+                H = self.H[...,i] # h need old
+                hback = np.diag(self.Hact.backward(pH).ravel())
                 self._gHH[...,i] = hback.dot\
                                    (self.HH.dot(self._gHH[...,i]))+\
                                    hback.dot\
@@ -186,12 +282,15 @@ class FCNN: # fully connected nueral network
 
 if __name__ == '__main__':
     # model
-    model = FCNN(1,30,1,sigmoid,time=30,bs=1)
+    model = FCNN(1,30,1,sigmoid,time=5,bs=1)
     # loss
     loss = se()
     # data
-    n = 2
+    n = 100
     data = Data(f=lambda x: [1] if x>n/2 else [0], n=n)
     data.shuffle()
+    # check: TODO: add check for from error to gradcheck
+    model.gradCheck()    
     # train
-    model.train(data.getTr(), loss)
+    # model.train(data.getTr(), loss)
+
